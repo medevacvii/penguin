6e145c5e1f40b989bb2cd791a873ccc5f3d64afd
Preston Yuen <55010594+medevacvii@users.noreply.github.com>
2025-08-03 15:19:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ with st.sidebar:
     'gender': gender
   }
 
-  input_df = pd.DataFrame(data, index[0])
+  input_df = pd.DataFrame(data, index = [0])
   input_penguins = pd.concat([input_df, X_raw], axis = 0)
 
 with st.expander("Input data"):
