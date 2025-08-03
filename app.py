30fedce42191bea6308a0750c9fb2c4a8406c0f8
Preston Yuen <55010594+medevacvii@users.noreply.github.com>
2025-08-03 15:39:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,88 @@
-import pandas as pd
 import streamlit as st
 import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+import altair as alt
 
-st.title("Penguin Species Prediction ML app")
-st.info("This is an end-to-end Machine Learning app")
+st.title('🤖 Machine Learning App Jazzpreet')
 
-with st.expander("Data"):
-  st.write("**Raw data**")
+st.info('This is app builds a machine learning model!')
+
+with st.expander('Data'):
+  st.write('**Raw data**')
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
-  
-  st.write("Input variables")
-  X_raw = df.drop('species', axis = 1)
+
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
   X_raw
-  
-  st.write("Target variable")
+
+  st.write('**y**')
   y_raw = df.species
   y_raw
 
-  st.write("Descriptive Statistics")
-  des = df.describe()
-  des
+with st.expander('Data visualization'):
+  #st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  
 
-  st.write("Mor information about Data")
-  info = df.info()
-  st.text(info)
+  chart = alt.Chart(df).mark_circle(size=60).encode(
+    x='bill_length_mm',
+    y='body_mass_g',
+    color='species',
+    tooltip=['species', 'bill_length_mm', 'body_mass_g']
+).interactive()
+  st.altair_chart(chart, use_container_width=True)
 
-
-with st.expander("Data Visualization"):
-  st.scatter_chart(data = df, x = 'bill_length_mm', y = 'body_mass_g', color = 'species')
-
+# Input features
 with st.sidebar:
-  st.header("Input Variables")
+  st.header('Input features')
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
-  bill_depth_mm = st.slider('Bill depth (mm)', 13.1,21.5,17.2)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender', ('male','female'))
-  data = {
-    'island':island,
-    'bill_length_mm': bill_length_mm,
-    'bill_depth_mm': bill_depth_mm,
-    'flipper_length_mm': flipper_length_mm,
-    'body_mass_g': body_mass_g,
-    'gender': gender
-  }
+  gender = st.selectbox('Gender', ('male', 'female'))
+  
+  # Create a DataFrame for the input features
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-  input_df = pd.DataFrame(data, index = [0])
-  input_penguins = pd.concat([input_df, X_raw], axis = 0)
-
-with st.expander("Input data"):
-  st.write("**Input data**")
+with st.expander('Input features'):
+  st.write('**Input penguin**')
   input_df
-  st.write("**Combined Data**")
+  st.write('**Combined penguins data**')
   input_penguins
 
-# One hot encoding for X
+
+# Data preparation
+# Encode X
 encode = ['island', 'sex']
-df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
 X = df_penguins[1:]
 input_row = df_penguins[:1]
 
-# One hot encoding for y
-target_mapper = {
-  'Adelie': 0,
-  'Chinstrap': 1,
-  'Gentoo': 2
-}
-
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
 def target_encode(val):
   return target_mapper[val]
 
-y = y_raw.apply(target_code)
+y = y_raw.apply(target_encode)
 
-with st.expander("Data Preparation"):
-  st.write("**Encoded X (Input Penguin)**")
+with st.expander('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
   input_row
-  st.write("Encoded Y")
+  st.write('**Encoded y**')
   y
+
 
 # Model training and inference
 ## Train the ML model
