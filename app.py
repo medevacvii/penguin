1d146ef13d1f3914f1f888bee03943217c0a9366
Preston Yuen <55010594+medevacvii@users.noreply.github.com>
2025-08-03 14:51:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,15 @@ with st.expander("Data"):
   st.write("**Raw data**")
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
   df
-
+  
+  st.write("Input variables")
+  X_raw = df.drop('species', axis = 1)
+  X_raw
+  
+  st.write("Target variable")
+  y_raw = df.species
+  y_raw
+  
 with st.expander("Data Visualization"):
   pass
 
