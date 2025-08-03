806799157abfbb87966d6060b181aec7a1cdab85
Preston Yuen <55010594+medevacvii@users.noreply.github.com>
2025-08-03 14:59:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ with st.expander("Data"):
   st.text(info)
   
 with st.expander("Data Visualization"):
-  pass
+  st.scatter_chart(data = df, x = 'bill_length_mm', y = 'body_mass_g', color = 'species')
 
 with st.expander("Input data"):
   pass
