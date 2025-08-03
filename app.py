3c332d537f03b7add0a58ae7978383c7ea8a92f3
Preston Yuen <55010594+medevacvii@users.noreply.github.com>
2025-08-03 14:30:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,20 @@ from sklearn.ensemble import RandomForestClassifier
 
 st.title("Lenguin Spicies Prediction ML app")
 st.info("This is an end-to-end Machine Learning app")
+
+with st.expander("Data"):
+  pass
+
+with st.expander("Data Visualization"):
+  pass
+
+with st.expander("Input data"):
+  pass
+
+with st.sidebar:
+  st.header("INput Variables")
+  island = st.selectionbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender',('male', 'female')
