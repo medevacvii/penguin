4c890fa58c7bc5403e660a1c05e84e1281ff5d27
Preston Yuen <55010594+medevacvii@users.noreply.github.com>
2025-08-03 14:40:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ with st.expander("Input data"):
   pass
 
 with st.sidebar:
-  st.header("INput Variables")
+  st.header("Input Variables")
   island = st.selectionbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
   flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender',('male', 'female')
+  gender = st.selectbox('Gender', ('male','female'))
