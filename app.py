14b174291cb14be4fbbe2ee3bec3eb70f4a3ac4a
Preston Yuen <55010594+medevacvii@users.noreply.github.com>
2025-08-03 15:26:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -59,3 +59,16 @@ with st.expander("Input data"):
   input_df
   st.write("**Combined Data**")
   input_penguins
+
+# One hot encoding for X
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+
+# One hot encoding for y
+target_mapper = {
+  'Adelie': 0,
+  'Chinstrap': 1,
+  'Gentoo': 2
+}
