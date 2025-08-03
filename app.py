83f4f1a3fb2db7e119555e4022ec1b0267680075
Preston Yuen <55010594+medevacvii@users.noreply.github.com>
2025-08-03 14:25:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
+st.title("Lenguin Spicies Prediction ML app")
+st.info("This is an end-to-end Machine Learning app")
